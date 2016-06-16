4ef3375eff124fef773ffc2e3910d1a0e619a986
automan0608 <zhaosheng@ubuntu.(none)>
2016-06-16 11:02:43 +0800

    add main-02.c

--- test-pthread/main-02.c
+++ test-pthread/main-02.c
@@ -4,6 +4,8 @@
 #include <pthread.h>
 #include <signal.h>
 
+static pthread_attr_t thread_attr;
+
 void *func(void *arg)
 {
     printf("in thread func, will pause\n");
@@ -18,7 +20,7 @@ void sigint_handle(int signo)
     printf("receive sigint, signo:%d\n", signo);
     
     pthread_t tid;
-    int result = pthread_create(&tid, NULL, func, NULL);
+    int result = pthread_create(&tid, &thread_attr, func, NULL);
     if (0 == result)
     {
         printf("create thread success!!\n");
@@ -31,7 +33,6 @@ void sigint_handle(int signo)
 
 int main()
 {
-    pthread_attr_t thread_attr;
     int result = pthread_attr_init(&thread_attr);
     if (0 != result)
     {
@@ -49,6 +50,19 @@ int main()
     {
         printf("get stack size error! \n");
     }
+    
+    stacksize = 2 * 1024 * 1024;
+    result = pthread_attr_setstacksize(&thread_attr, stacksize);
+    if (0 == result)
+    {
+        printf("set stack size %d B | %d kB | %d mB sucess!!\n\n", 
+                 stacksize, stacksize/1024, stacksize/1024/1024);
+    }
+    else
+    {
+        printf("set stack size error!!\n\n");
+    }
+
 
     printf("press ctrl+C to create a thread\n");
 
--- test-pthread/readme.md
+++ test-pthread/readme.md
@@ -1,3 +1,6 @@
 
-#main-01.c
+##main-01.c
     press ctrl+c to create thread, to observe the usage of resources
+
+##main-02.c
+    same as main-01.c, except set phread stack size to 2MB
