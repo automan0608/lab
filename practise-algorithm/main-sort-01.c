208b2b25f08e24daab3958c48cfdab640f07eb57
zhaosheng <zhaosheng@tp-link.net>
2016-12-16 17:40:02 +0800

    add practise-algorithm

---
+++ practise-algorithm/main-sort-01.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+static void printNums(int *sortnum, int num);
+static void mysort(int *sortnum, int num);
+
+int main(int argc, char const *argv[])
+{
+
+    printf("argc:%d\n", argc);
+    if (2 != argc)
+    {
+        printf("Usage: ./a.out num\n");
+        exit(-1);
+    }
+
+    int num;
+    int *sortnum;
+    int i;
+
+    num = atoi(argv[1]);
+    printf("there are %d nums will be random generate and be sort\n", num);
+
+    sortnum = (int *)malloc(num * sizeof(int));
+    memset(sortnum, 0, num * sizeof(int));
+
+    srandom(time(NULL));
+    for (i = 0; i < num; ++i)
+    {
+        sortnum[i] = random() % 100;
+    }
+
+    printNums(sortnum, num);
+
+    mysort(sortnum, num);
+
+    printNums(sortnum, num);
+
+    return 0;
+}
+
+static void printNums(int *sortnum, int num)
+{
+    int i;
+
+    for (i = 0; i < num; ++i)
+    {
+        printf("sortnum[%d] = %d\n", i, sortnum[i]);
+    }
+}
+
+// insert sort
+static void mysort(int *sortnum, int num)
+{
+    printf("in mysort\n");
+
+    int i = 0, j = 0;
+
+    for (i = 0; i < num; ++i)
+    {
+        for (j = 0; j < num - i -1; ++j)
+        {
+            if (sortnum[j] > sortnum[j + 1])
+            {
+                int tmp = sortnum[j];
+                sortnum[j] = sortnum[j + 1];
+                sortnum[j + 1] = tmp;
+            }
+        }
+    }
+}
+
+
+
